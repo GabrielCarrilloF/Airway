41e8455afcea98e7fab1033360ce24f7b10ba9f0
jesusbrave <jvaliente737@gmial.com>
2024-05-10 20:36:25 -0500

    la bestia hizo el login

---
+++ JavaScript/login.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        var modal = document.getElementById("modal-765830");
+        if (modal) {
+            // Simulamos el clic en el elemento para activar el modal
+            modal.click();
+        } else {
+            console.error("No se encontró el modal con el ID modal-765830.");
+        }
+    }, 10000); // Espera 5 segundos (5000 milisegundos)
+});
+
+
