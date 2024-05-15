453fdb44807c7c5e0be5c0c8e7be203be7c97bfa
Gabo.dev <carrillogabof@gmail.com>
2024-05-14 19:40:27 -0500

    El ACtualizar esta funcional listo

--- PHP/Actualizar.php
+++ PHP/Actualizar.php
@@ -123,7 +123,7 @@
     </div>
     <div id="Update" class="container my-5" style="display: none;">
       <center><h2>ACTUALIZAR</h2></center><br>
-      <form>
+      <form action="Update.php" method="post">
         <div class="row">
           <div class="col-md-6 mb-3">
             <label for="ID">
@@ -180,32 +180,14 @@
   <button type="submit" class="btn-enviar" id="openModal">Enviar</button>
   <button type="button" class="btn-enviar" onclick="FalseUpdate()">Canselar</button>
 
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-titulo" id="modaltitulo">Actualizacion Exitosa!</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p>¡La actualización se ha realizado correctamente!</p>
-        <img src="../Images/chulito verde.gif" alt="chulito verde" class="img-fluid">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn-cerrar" data-dismiss="modal">Cerrar</button>
-      </form>
-    </div>
-      </div>
-    </div>
-  </div>
-</div>
+
 </center>
+
+
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
+<script src="../JavaScript/alert.js"></script>
 <script src="../JavaScript/actualizar.js"></script>
 
 
